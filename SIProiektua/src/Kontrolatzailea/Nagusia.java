6affd7c8d1a62332100de66bb1021df27877f0c1
pablo <pablo@Pablo>
2026-03-04 18:40:43 +0100

    MVC patoia hobetuta

--- SIProiektua/src/Kontrolatzailea/Nagusia.java
+++ SIProiektua/src/Kontrolatzailea/Nagusia.java
@@ -8,12 +8,14 @@ public class Nagusia {
 		// EREDUA
 		JokoKudeatzailea jk = JokoKudeatzailea.getNireJK();
 		
-		// BISTA
-		JokoarenPanela jp = new JokoarenPanela();
-		jp.setVisible(true);
+		// BISTA(lehengo leihoa kargatu behar da)
+		//HasieraPanela hp = new HasieraPanela();
+        //hp.setVisible(true);
+		//JokoarenPanela jp = new JokoarenPanela();
+		//jp.setVisible(true);
 
-		//Jokoa hasi
-		jk.hasiJokoa();
+		//Jokoa hasi(HasieraPanela egingo du)
+		//jk.hasiJokoa();
 	}
 
 }
