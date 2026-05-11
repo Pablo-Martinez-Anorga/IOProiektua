c45b87dfb43a90899d46d1519390f5212d5a3ded
Andoni Cabrera <acabrera024@ikasle.ehu.eus>
2026-05-11 22:28:13 +0200

    Hasiera Leihoaren hobekuntza (retro estiloa, botoiak, atzealde beltza, ...)

--- SIProiektua/src/Bista/HasieraLeihoa.java
+++ SIProiektua/src/Bista/HasieraLeihoa.java
@@ -1,13 +1,20 @@
 package Bista;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,6 +22,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 import Eredua.Partida;
 import Kontrolatzailea.BotoiKontroladorea;
@@ -29,57 +37,136 @@ public class HasieraLeihoa extends JFrame implements Observer{
 	public HasieraLeihoa() {
 		setTitle("Space Invaders - Hasiera");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 400); 
+		setBounds(100, 100, 750, 500); 
+		setLocationRelativeTo(null); 
 		
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout());
+		contentPane.setBackground(Color.BLACK); 
+		contentPane.setBorder(new EmptyBorder(20, 20, 20, 20));
+		contentPane.setLayout(new BorderLayout(0, 20));
 		setContentPane(contentPane);
-		
-		JPanel panelErdian = new JPanel();
-		panelErdian.setLayout(new GridLayout(3, 1)); 
 		
 		// 1. Titulua
 		JPanel tituluPanela = new JPanel();
-		JLabel lblNewLabel = new JLabel("SPACE INVADERS");
-		lblNewLabel.setFont(new Font("Monospaced", Font.BOLD, 50));
-		tituluPanela.add(lblNewLabel);
-		panelErdian.add(tituluPanela); 
+		tituluPanela.setBackground(Color.BLACK);
+		JLabel lblIzenburua = new JLabel("SPACE INVADERS");
+		lblIzenburua.setForeground(Color.GREEN);
+		lblIzenburua.setFont(new Font(Font.MONOSPACED, Font.BOLD, 55));
+		tituluPanela.add(lblIzenburua);
+		contentPane.add(tituluPanela, BorderLayout.NORTH);
 		
-		// 2. Izena eta Ranking-a
+		
+		JPanel panelErdian = new JPanel();
+		panelErdian.setBackground(Color.BLACK);
+		panelErdian.setLayout(new GridLayout(2, 1, 0, 20)); 
+		
+		// 2. Izena eta Ranking-a 
 		JPanel datuPanela = new JPanel();
-		datuPanela.setLayout(new GridLayout(1, 2)); 
+		datuPanela.setBackground(Color.BLACK);
+		datuPanela.setLayout(new GridLayout(1, 2, 30, 0)); 
 		
+		// 2.1 Izenaren Panela
 		JPanel izenPanela = new JPanel();
-		izenPanela.add(new JLabel("Sartu zure izena: "));
-		txtIzena = new JTextField(12); 
+		izenPanela.setBackground(Color.BLACK);
+		izenPanela.setLayout(new BoxLayout(izenPanela, BoxLayout.Y_AXIS));
+		
+		JLabel lblIzena = new JLabel("SARTU ZURE IZENA:");
+		lblIzena.setForeground(Color.WHITE);
+		lblIzena.setFont(new Font(Font.MONOSPACED, Font.BOLD, 18));
+		lblIzena.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		txtIzena = new JTextField(12);
+		txtIzena.setMaximumSize(new Dimension(250, 40)); 
+		txtIzena.setBackground(Color.DARK_GRAY);
+		txtIzena.setForeground(Color.GREEN); 
+		txtIzena.setCaretColor(Color.WHITE); 
+		txtIzena.setFont(new Font(Font.MONOSPACED, Font.BOLD, 22));
+		txtIzena.setHorizontalAlignment(JTextField.CENTER);
+		txtIzena.setBorder(new LineBorder(Color.GREEN, 2)); 
+		txtIzena.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		izenPanela.add(Box.createVerticalGlue()); 
+		izenPanela.add(lblIzena);
+		izenPanela.add(Box.createVerticalStrut(10)); 
 		izenPanela.add(txtIzena);
+		izenPanela.add(Box.createVerticalGlue());
 		datuPanela.add(izenPanela);
 		
-		JTextArea txtTop = new JTextArea("--- TOP JOKALARIAK ---\n" + Partida.getNirePartida().getTopJokalariak());
+		// 2.2 Ranking Panela
+		JTextArea txtTop = new JTextArea("--- TOP JOKALARIAK ---\n\n" + Partida.getNirePartida().getTopJokalariak());
 		txtTop.setEditable(false);
-		txtTop.setBackground(this.getBackground());
+		txtTop.setBackground(Color.BLACK);
+		txtTop.setForeground(Color.YELLOW); 
+		txtTop.setFont(new Font(Font.MONOSPACED, Font.BOLD, 16));
+		txtTop.setBorder(new LineBorder(Color.YELLOW, 2)); 
+		txtTop.setMargin(new Insets(10, 10, 10, 10)); 
 		datuPanela.add(txtTop);
 		
 		panelErdian.add(datuPanela);
 		
-		// 3. Botoiak
-		JPanel botoiPanela = new JPanel();
-		btnGreen = new JButton("GREEN"); btnGreen.setActionCommand("G"); botoiPanela.add(btnGreen);
-		btnBlue = new JButton("BLUE"); btnBlue.setActionCommand("B"); botoiPanela.add(btnBlue);
-		btnRed = new JButton("RED"); btnRed.setActionCommand("R"); botoiPanela.add(btnRed);
+		// 3. Instrukzioak
+		JPanel instrukzioPanela = new JPanel();
+        instrukzioPanela.setBackground(Color.BLACK);
+        instrukzioPanela.setLayout(new BoxLayout(instrukzioPanela, BoxLayout.Y_AXIS));
+        
+        JLabel lblMugitu = new JLabel("Press <Arrows> to move");
+        lblMugitu.setForeground(Color.WHITE);
+        lblMugitu.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 18));
+        lblMugitu.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JLabel lblTiro = new JLabel("Press <Space> to shoot");
+        lblTiro.setForeground(Color.WHITE);
+        lblTiro.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 18));
+        lblTiro.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        instrukzioPanela.add(Box.createVerticalGlue());
+        instrukzioPanela.add(lblMugitu);
+        instrukzioPanela.add(Box.createVerticalStrut(10));
+        instrukzioPanela.add(lblTiro);
+        instrukzioPanela.add(Box.createVerticalGlue());
+        
+        panelErdian.add(instrukzioPanela);
+		contentPane.add(panelErdian, BorderLayout.CENTER);
 		
-		// Botoi berria aplikazioa erabat ixteko
-		btnExit = new JButton("IRTEN");
+		// 4. Botoiak 
+		JPanel botoiPanela = new JPanel();
+		botoiPanela.setBackground(Color.BLACK);
+		
+		btnGreen = sortuBotoiRetro("GREEN", Color.GREEN);
+		btnGreen.setActionCommand("G"); 
+		
+		btnBlue = sortuBotoiRetro("BLUE", new Color(0, 150, 255)); 
+		btnBlue.setActionCommand("B"); 
+		
+		btnRed = sortuBotoiRetro("RED", Color.RED);
+		btnRed.setActionCommand("R"); 
+		
+		btnExit = sortuBotoiRetro("IRTEN", Color.GRAY);
 		btnExit.setActionCommand("EXIT");
+		
+		botoiPanela.add(btnGreen);
+		botoiPanela.add(btnBlue);
+		botoiPanela.add(btnRed);
 		botoiPanela.add(btnExit);
 		
-		panelErdian.add(botoiPanela); 
-		contentPane.add(panelErdian, BorderLayout.CENTER);
+		contentPane.add(botoiPanela, BorderLayout.SOUTH); 
 		
 		this.setFocusable(true);
 		Partida.getNirePartida().addObserver(this);
 	}
+
+	// --- METODO LAGUNTZAILEA BOTOIAK ESTILIZATZEKO ---
+	private JButton sortuBotoiRetro(String testua, Color kolorea) {
+        JButton botoia = new JButton(testua);
+        botoia.setFont(new Font(Font.MONOSPACED, Font.BOLD, 22));
+        botoia.setForeground(kolorea);
+        botoia.setBackground(Color.DARK_GRAY);
+        botoia.setFocusPainted(false); 
+        botoia.setBorder(new LineBorder(kolorea, 3)); 
+        botoia.setPreferredSize(new Dimension(130, 50));
+        botoia.setCursor(new Cursor(Cursor.HAND_CURSOR)); 
+        return botoia;
+    }
 
 	public void setKontrolatzailea(BotoiKontroladorea bk) {
 		btnGreen.addActionListener(bk);
